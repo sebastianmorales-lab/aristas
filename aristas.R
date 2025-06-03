d3ba821eaed1128a3d907661e2a46784a67bd63f
sebastianmorales-lab <sebamorales@fcien.edu.uy>
2025-06-03 12:03:12 -0300

    Agrega graficas de distribucion remplaza valores de 99 por NA

--- aristas.R
+++ aristas.R
@@ -55,18 +55,28 @@ Habilidades_intrapersonales <- list(
   "ES7_1", "ES7_2", "ES7_3", "ES7_4", "ES7_5",
   "ES8_1-R", "ES8_2-R", "ES8_3-R", "ES8_4-R", "ES8_5-R", "ES8_6-R"
 )
+#----Reemplazo los valores de 99 en los items de las dimensiones por NA----
+# Reemplazo los valores de 99 en los items de las dimensiones por NA
+datos[, unlist(Motivacion_y_autorregulacion)][datos[, unlist(Motivacion_y_autorregulacion)] == 99] <- NA
+datos[, unlist(Habilidades_interpersonales)][datos[, unlist(Habilidades_interpersonales)] == 99] <- NA
+datos[, unlist(Habilidades_intrapersonales)][datos[, unlist(Habilidades_intrapersonales)] == 99] <- NA
+
+
 
 #----Calculo las dimensiones----
 
 # Calculo la suma de la dimensión Motivación y autorregulación
 suma_Motivacion_y_autorregulacion <- rowSums(datos[, unlist(Motivacion_y_autorregulacion)], na.rm = TRUE)
+suma_Motivacion_y_autorregulacion[rowSums(!is.na(datos[, unlist(Motivacion_y_autorregulacion)])) == 0] <- NA
 
 # Calculo la suma de la dimensión Habilidades interpersonales
 suma_Habilidades_interpersonales <- rowSums(datos[, unlist(Habilidades_interpersonales)], na.rm = TRUE)
+suma_Habilidades_interpersonales[rowSums(!is.na(datos[, unlist(Habilidades_interpersonales)])) == 0] <- NA
 
 # Calculo la suma de la dimensión Habilidades intrapersonales
 suma_Habilidades_intrapersonales <- rowSums(datos[, unlist(Habilidades_intrapersonales)], na.rm = TRUE)
-
+suma_Habilidades_intrapersonales[rowSums(!is.na(datos[, unlist(Habilidades_intrapersonales)])) == 0] <- NA
+#----Calculo las dimensiones promediando los items----
 # Calculo la dimensión Motivación y autorregulación
 dim_Motivacion_y_autorregulacion<- rowMeans(datos[, unlist(Motivacion_y_autorregulacion)], na.rm = TRUE)
 
@@ -89,6 +99,46 @@ print(head(datos))
 
 # Filtrar los estudiantes SIN Necesidades Educativas Especiales (NEE == 0)
 datos <- datos %>% filter(NEE == 0)
+
+# ver los valores únicos de cada dimensión
+print(unique(datos$Motivacion_y_autorregulacion))
+print(unique(datos$Habilidades_interpersonales))
+print(unique(datos$Habilidades_intrapersonales))
+# Ver valores únicos de cada ítem en la dimensión Motivación y autorregulación
+print(lapply(datos[, unlist(Motivacion_y_autorregulacion)], unique))
+# Ver valores únicos de cada ítem en la dimensión Habilidades interpersonales
+print(lapply(datos[, unlist(Habilidades_interpersonales)], unique))
+# Ver valores únicos de cada ítem en la dimensión Habilidades intrapersonales
+print(lapply(datos[, unlist(Habilidades_intrapersonales)], unique))
+
+# Grafico la distribución de las dimensiones
+print(
+  ggplot(datos, aes(x = Motivacion_y_autorregulacion)) +
+    geom_histogram(binwidth = 1, fill = "blue", color = "black", alpha = 0.7, na.rm = TRUE) +
+    geom_density(aes(y = ..count..), color = "red", size = 1, adjust = 1, na.rm = TRUE) +
+    labs(title = "Distribucion de Motivacion y Autorregulacion",
+         x = "Motivacion y Autorregulacion",
+         y = "Frecuencia") +
+    theme_minimal()
+)
+print(
+  ggplot(datos, aes(x = Habilidades_interpersonales)) +
+    geom_histogram(binwidth = 1, fill = "green", color = "black", alpha = 0.7, na.rm = TRUE) +
+    geom_density(aes(y = ..count..), color = "red", size = 1, adjust = 1, na.rm = TRUE) +
+    labs(title = "Distribucion de Habilidades Interpersonales",
+         x = "Habilidades Interpersonales",
+         y = "Frecuencia") +
+    theme_minimal()
+)
+print(
+  ggplot(datos, aes(x = Habilidades_intrapersonales)) +
+    geom_histogram(binwidth = 1, fill = "purple", color = "black", alpha = 0.7, na.rm = TRUE) +
+    geom_density(aes(y = ..count..), color = "red", size = 1, adjust = 1, na.rm = TRUE) +
+    labs(title = "Distribucion de Habilidades Intrapersonales",
+         x = "Habilidades Intrapersonales",
+         y = "Frecuencia") +
+    theme_minimal()
+)
 
 # Calculo el apha de Cronbach para cada dimensión
 alpha_Motivacion_y_autorregulacion <- psych::alpha(datos[, unlist(Motivacion_y_autorregulacion)], check.keys = TRUE)
